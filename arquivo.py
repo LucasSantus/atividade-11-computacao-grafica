66b6abb687ec010512f14b16ea8159e7f67780d7
Lucas Santus <leos9877@gmail.com>
2021-06-22 15:21:53 -0300

    inserindo alterações ao código

--- arquivo.py
+++ arquivo.py
@@ -1,57 +1,39 @@
 import numpy as np
 import cv2
 
-# Tamanho padrão da imagem
-largura = 500
-altura = 500
+def mostraVariavel(nome, email):
+    largura = 500
+    altura = 500
 
-def generated_image():
     # Cria uma nova imagem(500x500 e 3 canais RGB)
-    image = np.zeros((altura, largura, 3), dtype=np.uint8)
-    return image
+    imagem = np.zeros((altura, largura, 3), dtype=np.uint8)
+    x=0
+    y=0
 
-def mostraVariavel(imagem, nome, email):
     # Percorrendo a imagem
-    for y in range(0, altura):
-        for x in range(0, largura):
-            # if x > 250:
-            #     cv2.rectangle(imagem, (0, 0), (x, y), (0, 0, 0), -1)
-            # elif x < 250:
-            #     cv2.rectangle(imagem, (0, 0), (x, y), (0,255,0), -1)
+    for x in range(0, altura):
+        for y in range(0, largura):
+            if x <= altura/2:
+                imagem[x,y] = [0,0,0]
+            else:
+                imagem[x,y] = [0,255,0]
 
     # Desenha o texto com a variavel em preto, no centro
-    fonte = cv2.FONT_HERSHEY_SCRIPT_SIMPLEX
-    escala = 1
-    grossura = 2
+    fonte = cv2.FONT_HERSHEY_PLAIN
+    escala = 1.5
+    grossura = 1
 
     # Pega o tamanho (altura e largura) do texto em pixels
     tamanho, _ = cv2.getTextSize(nome, fonte, escala, grossura)
-
-    # Desenha o texto no centro
-    cv2.putText(imagem, nome, (int(largura / 2 - tamanho[0] / 2), 125), fonte, escala, (255, 255, 255), grossura)
-    cv2.putText(imagem, email, (int(largura / 2 - tamanho[0] / 2), 375), fonte, escala, (255, 255, 255), grossura)
-    # cv2.putText(imagem, texto, (int(largura / 2 - tamanho[0] / 2), 450), fonte, escala, (0, 0, 0), grossura)
+    
+    cv2.putText(imagem, nome, (int(largura / 2 - tamanho[0] / 2), int(altura / 2 / 2)), fonte, escala, (255, 255, 255), grossura)
+    cv2.putText(imagem, email, (int(largura / 2 - tamanho[0] / 2), int(altura - (altura / 2 / 2 ))), fonte, escala, (0, 0, 0), grossura)
     cv2.imwrite("teste.jpg", imagem)
 
+    inverter = cv2.flip(imagem, 1)
+    cv2.imwrite("testea.jpg", inverter)
+
 if __name__ == '__main__':
-    image = generated_image()
     nome = 'Lucas Eduardo de Oliveira Santos'
     email = 'lucas.oliveira@aluno.unincor.edu.br'
-    mostraVariavel(image, nome, email)
-
-# import cv2
-# import numpy as np
-# img=np.zeros((320,600),dtype=np.uint8)
-# cv2.putText(img,"FONT_HERSHEY_SIMPLEX",(10,30),cv2.FONT_HERSHEY_SIMPLEX,1,255)
-# cv2.putText(img,"FONT_HERSHEY_COMPLEX",(10,60),cv2.FONT_HERSHEY_COMPLEX,1,255)
-# cv2.putText(img,"FONT_HERSHEY_DUPLEX",(10,90),cv2.FONT_HERSHEY_DUPLEX,1,255)
-# cv2.putText(img,"FONT_HERSHEY_TRIPLEX",(10,120),cv2.FONT_HERSHEY_TRIPLEX,1,255)
-# cv2.putText(img,"FONT_HERSHEY_COMPLEX_SMALL",(10,150),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,255)
-# cv2.putText(img,"FONT_HERSHEY_PLAIN",(10,180),cv2.FONT_HERSHEY_PLAIN,1,255)
-# cv2.putText(img,"FONT_HERSHEY_SCRIPT_COMPLEX",(10,210),cv2.FONT_HERSHEY_SCRIPT_COMPLEX,1,255)
-# cv2.putText(img,"FONT_ITALIC",(10,240),cv2.FONT_ITALIC,1,255)
-# cv2.putText(img,"QT_FONT_BLACK",(10,270),cv2.QT_FONT_BLACK,1,255)
-# cv2.putText(img,"QT_FONT_NORMAL",(10,300),cv2.QT_FONT_NORMAL,1,255)
-# cv2.imshow("janela",img)
-# cv2.imwrite("fonte_opencv.jpg",img)
-# cv2.waitKey(0)
+    mostraVariavel(nome, email)
